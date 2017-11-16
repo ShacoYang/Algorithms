c5ac042c6273b19dead677912c9552cb78c3c7ec
Lu <ext-yang.lu@here.com>
2017-11-16 16:32:25 -0600

    MergeKsortedLists array, list

--- src/LeetcodeNotes/Linked_list/MergeKSortedLists.java
+++ src/LeetcodeNotes/Linked_list/MergeKSortedLists.java
@@ -1,4 +1,43 @@
 package LeetcodeNotes.Linked_list;
 
 public class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists){
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return patition(lists, 0, lists.length - 1);
+    }
+    public ListNode patition(ListNode[] lists, int start, int end) {
+        if (start == end){
+            return lists[start];
+        } else if(start < end) {
+            int mid = start + (end - start) / 2;
+            ListNode left = patition(lists, start, mid);
+            ListNode right = patition(lists, mid + 1, end);
+            return merge(left, right);
+        } else {
+            return null;
+        }
+
+    }
+    public ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+        if (list1 != null){
+            tail.next = list1;
+        }else {
+            tail.next = list2;
+        }
+    return dummy.next;
+    }
 }
